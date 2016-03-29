e4091cb5a76b1a9c1b58c8cbefd104549805ae8a
jmartin <jmartin@zikzakmedia.com>
2016-03-29 12:00:40 +0200

    Add python 3 support

--- sale.py
+++ sale.py
@@ -2,13 +2,12 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import ModelView, fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import Pool
 from trytond.pyson import Eval, If
 from trytond.transaction import Transaction
 from trytond.wizard import Button, StateTransition, StateView, Wizard
 
 __all__ = ['SaleChangePartyStart', 'SaleChangeParty']
-__metaclass__ = PoolMeta
 
 
 class SaleChangePartyStart(ModelView):
